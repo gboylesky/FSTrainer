c327030b38107b3a1ad10b4fc99ea27bb3f31f94
gboylesky <Graham_Boyles@yahoo.com.au>
2025-11-04 17:42:29 +1100

    Add No Hint toggle

--- Randoms.py
+++ Randoms.py
@@ -5,6 +5,13 @@ from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
+@app.after_request
+def add_no_cache_headers(response):
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
+
 # ---------------------------
 # Configuration
 # ---------------------------
